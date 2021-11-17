e2af41cbde80a03727be55762fa8b0f6bad9cf26
Tadhg <tadhgsmboyle@gmail.com>
2021-11-17 09:22:53 -0800

    fix indentation in adminController

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,21 +1,21 @@
 const { userModel } = require("../database");
+
 const adminController = {
 
-        listSessions: (req, res) => {
+    listSessions: (req, res) => {
 
-            // gets all the active sessions and parses(converts) them into a object
-            const parseSession = JSON.parse(JSON.stringify(req.sessionStore.sessions))
-            res.render('admin/index', {
-                sessions: parseSession,
-                userModel: userModel,
-            });
-        },
+        // gets all the active sessions and parses(converts) them into a object
+        const parseSession = JSON.parse(JSON.stringify(req.sessionStore.sessions))
+        res.render('admin/index', {
+            sessions: parseSession,
+            userModel: userModel,
+        });
+    },
 
-        revokeSession: (req, res) => {
-            req.sessionStore.destroy(req.params.sessionId);
-            res.redirect('/admin');
-        }
+    revokeSession: (req, res) => {
+        req.sessionStore.destroy(req.params.sessionId);
+        res.redirect('/admin');
+    },
+}
 
-    }
-
-    module.exports = adminController;
+module.exports = adminController;
