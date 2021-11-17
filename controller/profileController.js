775f7771224dcebcb4d765f91a17ceb0a9ac9ac4
chris <77702776+cchan45@users.noreply.github.com>
2021-11-16 21:28:17 -0800

    made changes

--- controller/profileController.js
+++ controller/profileController.js
@@ -4,20 +4,24 @@ const axios = require('axios').default;
 
 let profileController = {
     displayProfile: (req, res) => {
-        if (req.user.ppic === undefined){ //if user doesn't have a profile picture in the database (usually for users who are registering)
-            axios.get(`https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS}`) //get request for a profile picture
+        //if user doesn't have a profile picture in the database (usually for users who are registering)
+        if (req.user.profile_picture === undefined){ 
+            //get request for a profile picture
+            axios.get(`https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS}`) 
               .then((response) => {
-                  const searchUser = Database.find(user => user.id === req.user.id) //finds the user in the database
-                  searchUser['ppic'] = response.data.urls.small //adds the picture link (from UnsplashAPI) into their account
-                  res.render('profile/profile', { user: req.user }) //renders their profile with a random profile picture
+                  //finds the user in the database
+                  const searchUser = Database.find(user => user.id === req.user.id) 
+                  //adds the picture link (from UnsplashAPI) into their account
+                  searchUser['profile_picture'] = response.data.urls.small 
+                  //renders their profile with a random profile picture
+                  res.render('profile/profile') 
               })
               .catch((err) => res.render(err))
-        } else { //if a user has a picture in the database already
-            res.render('profile/profile', { user: req.user })
+        } else { 
+            //if a user has a picture in the database already
+            res.render('profile/profile')
         }
     }, 
-    //add another function here if needed
-
 };
 
 module.exports = profileController
